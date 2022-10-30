2c8ca4e795217741c9d6deb96c1855a82009ee6d
Rima Das <rimadas.automationtester@gmail.com>
2022-10-30 15:33:17 -0700

    routes added to product

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -36,17 +36,14 @@ router.get('/:id', async(req, res) => {
 });
 
 // create new product
-router.post('/', async (req, res) => {
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
-  Product.create(req.body)
-    .then((product) => {
+router.post('/', (req, res) => {
+  Product.create({
+      product_name: req.params.product_name,
+      price: req.params.price,
+      stock: req.params.stock,
+      category_id: req.params.category_id,
+      tagIds: req.params.tagIds
+    }).then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
         const productTagIdArr = req.body.tagIds.map((tag_id) => {
@@ -109,8 +106,21 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+router.delete('/:id', async (req, res) => {
+  try{
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    if(!productData){
+      res.status(404).json({message: 'No Product found with this id'});
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err){
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
